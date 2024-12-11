87c228dbe6ab269bdb5c27f74bcadbeea4cd1a0b
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-11 04:24:38 +0100

    modifing the model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',
+        'adresse',
+        'image',
+        'role',
+        'email_verified_at',
     ];
 
     /**
