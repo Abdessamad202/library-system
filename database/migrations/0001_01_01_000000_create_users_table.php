15289d80140db5c5c1cf25b2f82d82781111e41f
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-11 04:25:38 +0100

    new user migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 100);
+            $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('image', 100)->default("user-profile.webp");
+            $table->string('phone', 20)->nullable();
+            $table->string('adresse', 100)->nullable();
+            $table->string('password', 255);
             $table->string('role')->default("user");
             // $table->rememberToken();
             $table->timestamps();
