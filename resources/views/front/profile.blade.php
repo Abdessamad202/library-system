0382946d32bb6fa7083cbb0493c086d0b29b022d
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-11 04:29:12 +0100

    modifing the profile file

--- resources/views/front/profile.blade.php
+++ resources/views/front/profile.blade.php
@@ -13,27 +13,28 @@
                     <div class="card-header bg-primary text-white">
                         <h2 class="h2">Profile Information</h2>
                     </div>
-                    <div class="card-body d-flex flex-column justify-content-evenly">
+                    <div class="card-body d-flex flex-column align-content-center justify-content-evenly">
                         <div class="row mb-3">
                             <div class="col-sm-3 text-muted">Full Name</div>
-                            <div class="col-sm-9">John Doe</div>
+                            <div class="col-sm-9">{{auth()->user()->name}}</div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-sm-3 text-muted">Email</div>
-                            <div class="col-sm-9">johndoe@example.com</div>
+                            <div class="col-sm-9">{{auth()->user()->email}}</div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-sm-3 text-muted">Phone</div>
-                            <div class="col-sm-9">+1 234 567 890</div>
+                            <div class="col-sm-9">{{auth()->user()->phone === null ? "Null" : auth()->user()->phone}}</div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-sm-3 text-muted">Address</div>
-                            <div class="col-sm-9">123 Main Street, Cityville, Country</div>
+                            <div class="col-sm-9">{{auth()->user()->adresse === null ? "Null" : auth()->user()->adresse}}</div>
                         </div>
                     </div>
                     <div class="card-footer text-end">
-                        <button class="btn btn-secondary">Edit Profile</button>
+                        <button class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#editProfileModal">Edit Profile</button>
                     </div>
+                    <x-modal />
                 </div>
             </div>
         </div>
