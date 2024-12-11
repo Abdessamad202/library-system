a99d395ec7db63fb44705a55ef0bcb1a21b4bcd2
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-11 04:28:48 +0100

    new routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', function () {
-    return view('connect');
-})->name("home");
-Route::get('/login', [App\Http\Controllers\UserController::class,"loginView"]);
-Route::post('/register', [App\Http\Controllers\UserController::class,"register"])->name("register");
-Route::post('/login', [App\Http\Controllers\API\UserController::class,"login"])->name("login");
+Route::get('/home', [AuthController::class, "home"])->name("home")->middleware("auth");
+Route::get('/profile', [AuthController::class, "profile"])->name("profile")->middleware("auth");
+Route::post('/profile', [App\Http\Controllers\UserController::class, "updateProfile"])->name("profile.update")->middleware("auth");
+Route::get('/login', [App\Http\Controllers\UserController::class, "loginView"]);
+Route::post('/register', [App\Http\Controllers\UserController::class, "register"])->name("register");
+Route::post('/login', [App\Http\Controllers\API\UserController::class, "login"])->name("login");
 Route::post('/logout', [App\Http\Controllers\UserController::class, 'logout'])->name('logout');
 Route::view("/layout", "front.home")->name("layout");
 Route::view("/books", "front.books-search")->name("books");
