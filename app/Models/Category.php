06fe1d4bde08e788eaa7d3b37c70be162ac8e3d1
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-13 03:34:53 +0100

    Update Book and Category models with necessary relationships

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,9 +5,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class Category extends Model
 {
     // Define the fillable properties
+    use HasFactory;
     protected $fillable = ["name", "description"];
 
     // Define the relationship with the Book model
