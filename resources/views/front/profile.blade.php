60f21c6767b5428a0b225f1e6d054b82b094e077
abdessamad <[200~git config --global user.email kechaf.abdo.ka.2018@gmail.com>
2024-12-16 03:02:22 +0100

    Updated profile.blade.php

--- resources/views/front/profile.blade.php
+++ resources/views/front/profile.blade.php
@@ -5,6 +5,9 @@
 @endsection
 @section('content')
     <x-navbar />
+    @session('success')
+        <x-notification message="{{ session('success') }}" />
+    @endsession
     <x-sidebar>
         <div class=" row  w-100 padding-100   mt-5 mb-5 ">
             <div class=" px-4 h-100-vh d-flex justify-content-center align-items-center flex-column">
@@ -16,28 +19,31 @@
                     <div class="card-body d-flex flex-column align-content-center justify-content-evenly">
                         <div class="row mb-3">
                             <div class="col-sm-3 text-muted">Full Name</div>
-                            <div class="col-sm-9">{{auth()->user()->name}}</div>
+                            <div class="col-sm-9">{{ auth()->user()->name }}</div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-sm-3 text-muted">Email</div>
-                            <div class="col-sm-9">{{auth()->user()->email}}</div>
+                            <div class="col-sm-9">{{ auth()->user()->email }}</div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-sm-3 text-muted">Phone</div>
-                            <div class="col-sm-9">{{auth()->user()->phone === null ? "Null" : auth()->user()->phone}}</div>
+                            <div class="col-sm-9">{{ auth()->user()->phone === null ? 'Null' : auth()->user()->phone }}
+                            </div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-sm-3 text-muted">Address</div>
-                            <div class="col-sm-9">{{auth()->user()->adresse === null ? "Null" : auth()->user()->adresse}}</div>
+                            <div class="col-sm-9">{{ auth()->user()->adresse === null ? 'Null' : auth()->user()->adresse }}
+                            </div>
                         </div>
                     </div>
                     <div class="card-footer text-end">
-                        <button class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#editProfileModal">Edit Profile</button>
+                        <button class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#editProfileModal">Edit
+                            Profile</button>
                     </div>
                     <x-modal />
                 </div>
             </div>
         </div>
-    </x-sidebar >
+    </x-sidebar>
     <x-footer />
 @endsection
