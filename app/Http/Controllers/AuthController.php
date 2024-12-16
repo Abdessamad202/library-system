ce043b4ac7e45a398c5c91d7cf89a4e5cfe930c1
abdessamad <[200~git config --global user.email kechaf.abdo.ka.2018@gmail.com>
2024-12-16 02:59:16 +0100

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ use App\Models\Book;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Models\ReservationBooks;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
@@ -27,6 +28,15 @@ class AuthController extends Controller
     }
     public function bookView(Book $book){
         // return dd($book);
+        $book->search = false ;
+        $reservations= Auth::user()->reservations->where('state', 'pending');
+        foreach ($reservations as $reservation) {
+            $ReservationBooks = ReservationBooks::where('reservation_id', $reservation->id)->get();
+            $book_id = Book::find($ReservationBooks[0]->book_id)-;
+            if($book_id == $book->id){
+                $book->search = true ;
+            }
+        }
         return view('front.book', compact('book'));
     }
     public function categoryView(Category $category){
