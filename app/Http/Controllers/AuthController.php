5afc7208e4db5833c14c17177499a463a0892185
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-17 19:31:45 +0100

    Updated AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,16 +29,20 @@ class AuthController extends Controller
     }
     public function bookView(Book $book){
         // return dd($book);
-        $book->search = false ;
+        $book['search'] = false ;
         $reservations= Auth::user()->reservations->where('state', 'pending');
         foreach ($reservations as $reservation) {
             $ReservationBooks = ReservationBooks::where('reservation_id', $reservation->id)->get();
             $book_id = Book::find($ReservationBooks[0]->book_id)->id;
             if($book_id == $book->id){
-                $book->search = true ;
+                $book['search'] = true ;
             }
         }
-        return view('front.book', compact('book'));
+        $relatedBooks = $book->category->books->take(5)->toArray();
+        // $book = $book->toArray();
+        // return dd($related);
+        // return dd($book,$relatedBooks);
+        return view('front.book',compact('relatedBooks','book'));
     }
     public function categoryView(Category $category){
         // return dd($category);
