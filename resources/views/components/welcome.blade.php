8f91cee844b3997ce01606be9030cf43a03eecb4
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-19 12:36:06 +0100

    Updated welcome.blade.php in resources/views/components

---
+++ resources/views/components/welcome.blade.php
@@ -0,0 +1,14 @@
+<div class="welcome bg-white rad-10 txt-c-mobile block-mobile">
+    <div class="intro p-20 d-flex space-between bg-eee">
+      <div>
+        <h2 class="m-0">Welcome</h2>
+        <p class="c-grey mt-5">Abdessamad</p>
+      </div>
+      <img class="hide-mobile" src="../imgs/welcome.png" alt="" />
+    </div>
+    <img src="../imgs/avatar.png" alt="" class="avatar" />
+    <div class="body txt-c d-flex p-20 mt-20 mb-20 block-mobile">
+      <div>Abdessamad Kechchaf<span class="d-block c-grey fs-14 mt-10">Developer</span></div>
+    </div>
+    <a href="profile.html" class="visit d-block fs-14 bg-blue c-white w-fit btn-shape">Profile</a>
+  </div>
