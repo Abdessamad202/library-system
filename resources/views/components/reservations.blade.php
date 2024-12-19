ec05934dcabbbbbc382b56f46f463f6deea2a94a
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-19 12:36:05 +0100

    Updated reservations.blade.php in resources/views/components

---
+++ resources/views/components/reservations.blade.php
@@ -0,0 +1,63 @@
+<div class="projects p-20 bg-white rad-10 m-20">
+    <h2 class="mt-0 mb-20">Projects</h2>
+    <div class="responsive-table">
+        <table class="fs-15 w-full">
+            <thead>
+                <tr>
+                    <td>User</td>
+                    <td>Book</td>
+                    <td>Cover</td>
+                    <td>Finish Date</td>
+                    <td>Operation</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Ministry Wikipedia</td>
+                    <td>Ministry</td>
+                    <td>$5300</td>
+                    <td>10 May 2022</td>
+                    <td>
+                        <span class="label btn-shape bg-green c-white">Accepte Reservation</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Elzero Shop</td>
+                    <td>Elzero Company</td>
+                    <td>$1500</td>
+                    <td>12 Oct 2021</td>
+                    <td><span class="label btn-shape bg-green c-white">Accepte Reservation</span></td>
+                </tr>
+                <tr>
+                    <td>Bouba App</td>
+                    <td>Bouba</td>
+                    <td>$800</td>
+                    <td>05 Sep 2021</td>
+                    <td><span class="label btn-shape bg-green c-white">Accepte Reservation</span></td>
+                </tr>
+                <tr>
+                    <td>Mahmoud Website</td>
+                    <td>Mahmoud</td>
+                    <td>$600</td>
+                    <td>22 May 2021</td>
+                    <td><span class="label btn-shape bg-green c-white">Accepte Reservation</span></td>
+                </tr>
+                <tr>
+                    <td>Sayed Website</td>
+                    <td>Sayed</td>
+                    <td>$300</td>
+                    <td>24 May 2021</td>
+
+                    <td><span class="label btn-shape bg-green c-white">Accepte Reservation</span></td>
+                </tr>
+                <tr>
+                    <td>Arena Application</td>
+                    <td>Arena Company</td>
+                    <td>$2600</td>
+                    <td>01 Mar 2021</td>
+                    <td><span class="label btn-shape bg-green c-white">Accepte Reservation</span></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
