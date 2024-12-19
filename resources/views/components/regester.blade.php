3bec773bec228e0dddd29a2516247ca7db80a8d9
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-19 12:36:02 +0100

    Updated regester.blade.php in resources/views/components

---
+++ resources/views/components/regester.blade.php
@@ -0,0 +1,43 @@
+<div class="projects p-20 bg-white rad-10 m-20">
+    <h2 class="mt-0 mb-20">Last Regestration</h2>
+    <div class="responsive-table">
+        <table class="fs-15 w-full">
+            <thead>
+                <tr>
+                    <td>Name</td>
+                    <td>email</td>
+                    <td>profile</td>
+                    <td>Date Regestration</td>
+                </tr>
+            </thead>
+            <!-- Fake Data -->
+            <tbody>
+                <tr>
+                    <td>Ministry Wikipedia</td>
+                    <td>Ministry</td>
+                    <td>$5300</td>
+                    <td>10 May 2022</td>
+                </tr>
+                <tr>
+                    <td>Elzero Shop</td>
+                    <td>Elzero Company</td>
+                    <td>$1500</td>
+                    <td>12 Oct 2021</td>
+                </tr>
+                <tr>
+                    <td>Bouba App</td>
+                    <td>Bouba</td>
+                    <td>$800</td>
+                    <td>05 Sep 2021</td>
+                </tr>
+                <tr>
+                    <td>Mahmoud Website</td>
+                    <td>Mahmoud</td>
+                    <td>$600</td>
+                    <td>22 May 2021</td>
+                </tr>
+            </tbody>
+            <tbody>
+        </table>
+    </div>
+</div>
