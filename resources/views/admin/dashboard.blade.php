099182ce78e9ed4d3743b305d36355d2277adc70
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-20 00:43:32 +0100

    Updated dashboard.blade.php in resources/views/admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,6 +1,15 @@
-@extends('../layout.admin-layout' )
+@extends('../layout.admin-layout')
 @section('title', 'Dashboard')
 @section('content')
-  <x-admin-sidebar/>
-  <x-content title="Dashboard"/>
+    <x-admin-sidebar />
+    <div class="content w-full">
+        <x-head />
+        <h1 class="p-relative">Dashboard</h1>
+        <x-wrapper>
+            <x-welcome />
+            <x-books-info />
+        </x-wrapper>
+        <x-regester />
+        <x-reservations />
+    </div>
 @endsection
