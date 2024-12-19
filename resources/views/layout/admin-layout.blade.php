8eee06d0c1e8a4f78021db6d90a165704a579992
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-19 12:36:08 +0100

    Updated admin-layout.blade.php in resources/views/layout

---
+++ resources/views/layout/admin-layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  @vite(['resources/css/all.min.css', 'resources/css/framework.css','resources/css/master.css'])
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;500&display=swap" rel="stylesheet" />
+  <title>@yield('title')</title>
+</head>
+<body>
+  <div class="page d-flex p-relative ">
+    <h1 class="p-relative">@yield('title') </h1>
+    @yield('content')
+  </div>
+</body>
+</html>
