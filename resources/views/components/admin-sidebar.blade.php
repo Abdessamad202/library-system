0f7a9e4cf3389b6983b27f5180fbe12d08eea283
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-20 00:43:34 +0100

    Updated admin-sidebar.blade.php in resources/views/components

--- resources/views/components/admin-sidebar.blade.php
+++ resources/views/components/admin-sidebar.blade.php
@@ -2,33 +2,33 @@
     <h3 class="p-relative txt-c mt-0"><span class='c-red'>A</span>IBRARY</h3>
     <ul>
       <li>
-        <a class="active d-flex align-center fs-14 c-black rad-6 p-10" href="index.html">
+        <a class="{{ request()->routeIs('admin.dashboard') ? 'active' : ''}} d-flex align-center fs-14 c-black rad-6 p-10" href="{{route('admin.dashboard')}}">
           <i class="fa-regular fa-chart-bar fa-fw"></i>
           <span>Dashboard</span>
         </a>
       </li>
       <li>
-        <a class="d-flex align-center fs-14 c-black rad-6 p-10" href="settings.html">
+        <a class="{{ request()->routeIs('admin.settings') ? 'active' : ''}} d-flex align-center fs-14 c-black rad-6 p-10" href="">
           <i class="fa-solid fa-gear fa-fw"></i>
           <span>Settings</span>
         </a>
       </li>
       <li>
-        <a class="d-flex align-center fs-14 c-black rad-6 p-10" href="profile.html">
+        <a class="{{ request()->routeIs('admin.profile') ? 'active' : ''}} d-flex align-center fs-14 c-black rad-6 p-10" href="">
           <i class="fa-regular fa-user fa-fw"></i>
           <span>Profile</span>
         </a>
       </li>
       <li>
-        <a class="d-flex align-center fs-14 c-black rad-6 p-10" href="projects.html">
+        <a class="{{ request()->routeIs('admin.books') ? 'active' : ''}} d-flex align-center fs-14 c-black rad-6 p-10" href="{{route('admin.books')}}">
           <i class="fa-solid fa-diagram-project fa-fw"></i>
-          <span>Projects</span>
+          <span>Books</span>
         </a>
       </li>
       <li>
-        <a class="d-flex align-center fs-14 c-black rad-6 p-10" href="courses.html">
+        <a class="{{ request()->routeIs('admin/reservations') ? 'active' : ''}} d-flex align-center fs-14 c-black rad-6 p-10" href="courses.html">
           <i class="fa-solid fa-graduation-cap fa-fw"></i>
-          <span>Courses</span>
+          <span>Reservations</span>
         </a>
       </li>
       <li>
