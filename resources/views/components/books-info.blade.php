67c0f8f42b973906e5a21fc49d75f2a0ca879e62
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-22 22:47:32 +0100

    Updated books-info.blade.php in resources/views/components

--- resources/views/components/books-info.blade.php
+++ resources/views/components/books-info.blade.php
@@ -1,26 +1,27 @@
+@props(['statistics'])
 <div class="tickets p-20 bg-white rad-10">
     <h2 class="mt-0 mb-10">Books Statistics</h2>
     <p class="mt-0 mb-20 c-grey fs-15">Everything About Support Books</p>
     <div class="d-flex txt-c gap-20 f-wrap">
       <div class="box p-20 rad-10 fs-13 c-grey">
         <i class="fa-solid fa-book-open fa-2x mb-10 c-orange"></i>
-        <span class="d-block c-black fw-bold fs-25 mb-5 count">100</span>
+        <span class="d-block c-black fw-bold fs-25 mb-5 count">{{ $statistics['all'] }}</span>
         Total
       </div>
       <div class="box p-20 rad-10 fs-13 c-grey">
         <i class="fa-solid fa-spinner fa-2x mb-10 c-blue"></i>
-        <span class="d-block c-black fw-bold fs-25 mb-5 count">50</span>
+        <span class="d-block c-black fw-bold fs-25 mb-5 count">{{ $statistics['pending'] }}</span>
         Pending
       </div>
       <div class="box p-20 rad-10 fs-13 c-grey">
         <i class="fa-regular fa-circle-check fa-2x mb-10 c-green"></i>
-        <span class="d-block c-black fw-bold fs-25 mb-5 count">10</span>
-        Closed
+        <span class="d-block c-black fw-bold fs-25 mb-5 count">{{ $statistics['reserved'] }}</span>
+        Reserved
       </div>
       <div class="box p-20 rad-10 fs-13 c-grey">
         <i class="fa-regular fa-rectangle-xmark fa-2x mb-10 c-red"></i>
-        <span class="d-block c-black fw-bold fs-25 mb-5 count">20</span>
-        Deleted
+        <span class="d-block c-black fw-bold fs-25 mb-5 count">{{ $statistics['cancelled'] }}</span>
+        Cancelled
       </div>
     </div>
   </div>
