007391dcb6e5ae364d26fa7aea71724b5e31240b
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-22 22:47:29 +0100

    Updated 2024_12_01_021006_create_books_table.php in database/migrations

--- database/migrations/2024_12_01_021006_create_books_table.php
+++ database/migrations/2024_12_01_021006_create_books_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained('categories');
             $table->string('title');
             $table->string('author');
             $table->string('edition');
@@ -21,7 +22,7 @@ return new class extends Migration {
             $table->string('image');
             $table->date('date_edition');
             $table->integer('stock');
-            $table->foreignId('category_id')->constrained('categories');
+            $table->integer('reserved_number')->default(0);
             $table->timestamps();
             $table->softDeletes();
         });
