b3caa107e99602bff34e7051d5d925b02316dfaa
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-22 22:47:26 +0100

    Updated ReservationController.php in app/Http/Controllers

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,47 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use App\Models\Reservation;
 use Illuminate\Http\Request;
+use App\Models\ReservationBooks;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\ReserveRequest;
-use App\Models\ReservationBooks;
 
 class ReservationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(ReserveRequest $request)
     {
-        Reservation::create(["date_emprunt" => $request->date_emprunt,"hour_emprunt" => $request->hour_emprunt,"user_id" => Auth::user()->id]);
-        //
-        ReservationBooks::create(["book_id" => $request->book_id,"reservation_id" => Reservation::latest()->first()->id]);
+        Reservation::create(["date_emprunt" => $request->date_emprunt,"hour_emprunt" => $request->hour_emprunt,"user_id" => Auth::user()->id,"book_id" => $request->book_id,]);
+        // ReservationBooks::create([,"reservation_id" => Reservation::latest()->first()->id]);
+        $book = Book::find($request->book_id);
+        $book->reserved_number +=1;
+        // $book->reserved = true;
+        $book->save();
         return response()->json(["success" => true,"message" => "Reservation created successfully"]);
     }
     public function cancel(Reservation $reservation)
     {
+        $book =Book::find($reservation->book_id);
+        $book->reserved_number -= 1;
+        $book->save();
+        // return dd();
         $reservation->state = "cancelled";
         $reservation->save();
         return redirect()->route("reservation")->with("success","Reservation cancelled successfully");
+    }
+    public function reserved(Reservation $reservation){
+        $reservation->state = "reserved";
+        $reservation->save();
+        return redirect()->route("admin.dashboard")->with("success","Reservation reserved successfully");
     }
     /**
      * Display the specified resource.
