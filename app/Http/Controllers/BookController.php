3b4b1913fd0970d42036aacf19e1a10a25276e88
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-24 20:54:14 +0100

    Updated BookController.php in app/Http/Controllers

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -8,28 +8,6 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    // public function index()
-    // {
-    //     //
-    //     $books = Book::all();
-
-    // }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    // public function create()
-    // {
-    //     //
-    //     return view();
-    // }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(BookRequest $request)
     {
         Book::create($request->validated());
@@ -39,40 +17,13 @@ class BookController extends Controller
         $book->restore();
         return redirect()->route('admin.books')->with('success','Book restored successfully');
     }
-    /**
-     * Display the specified resource.
-     */
-    // public function show(Book $book)
-    // {
-    //     //
-    //     return view("",compact("book"));
-    // }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    // public function edit(Book $book)
-    // {
-    //     //
-    //     return view();
-    // }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(BookRequest $request, Book $book)
     {
-        //
         $book->update($request->validated());
         return redirect()->route('admin.books')->with('success','Book updated successfully');
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Book $book)
     {
-        //
         $book->delete();
         return redirect()->route('admin.books')->with('success','Book deleted successfully');
     }
