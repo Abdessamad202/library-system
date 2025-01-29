c86df9c954f90c23373e5304f3d5c5c51d2e973f
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-29 23:00:18 +0100

    Updated welcome component for homepage

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -2,13 +2,13 @@
     <div class="intro p-20 d-flex space-between bg-eee">
       <div>
         <h2 class="m-0">Welcome</h2>
-        <p class="c-grey mt-5">Abdessamad</p>
+        <p class="c-grey mt-5">{{auth()->user()->name}}</p>
       </div>
       <img class="hide-mobile" src="../imgs/welcome.png" alt="" />
     </div>
     <img src="../imgs/avatar.png" alt="" class="avatar" />
     <div class="body txt-c d-flex p-20 mt-20 mb-20 block-mobile">
-      <div>Abdessamad Kechchaf<span class="d-block c-grey fs-14 mt-10">Developer</span></div>
+      <div>{{auth()->user()->name}}<span class="d-block c-grey fs-14 mt-10">{{auth()->user()->role}}</span></div>
     </div>
     <a href="profile.html" class="visit d-block fs-14 bg-blue c-white w-fit btn-shape">Profile</a>
   </div>
