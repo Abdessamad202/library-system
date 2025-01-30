0f84b982a1ed0e0052dec6532fa2959a890640f0
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-30 19:34:47 +0100

    Update admin profile view with modal and dynamic changes

---
+++ resources/views/admin/profile.blade.php
@@ -0,0 +1,18 @@
+@extends('../layout.admin-layout')
+@section('title', 'Dashboard')
+@section('content')
+    <x-admin-sidebar />
+    <div class="content w-full">
+        <x-head />
+        {{-- @dd($reservations) --}}
+        <h1 class="p-relative">Profile</h1>
+        <div class="p-10">
+            <x-welcome />
+        </div>
+
+        {{-- <x-reservations :reservations='$reservations' /> --}}
+    </div>
+    @session('success')
+        <x-notification message="{{ session('success') }}" />
+    @endsession
+@endsection
