60d8108745cf36e743e9fca5b58dafdd3dbf5c1c
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-30 19:34:47 +0100

    Update admin sidebar component with profile link

--- resources/views/components/admin-sidebar.blade.php
+++ resources/views/components/admin-sidebar.blade.php
@@ -1,41 +1,56 @@
 <div class="sidebar bg-white p-20 p-fixed h-100-vh z-index-10">
     <h3 class="p-relative txt-c mt-0"><span class='c-red'>A</span>IBRARY</h3>
     <ul>
-      <li>
-        <a class="{{ request()->routeIs('admin.dashboard') ? 'active' : ''}} d-flex align-center fs-14 c-black rad-6 p-10" href="{{route('admin.dashboard')}}">
-          <i class="fa-regular fa-chart-bar fa-fw"></i>
-          <span>Dashboard</span>
-        </a>
-      </li>
-      <li>
-        <a class="{{ request()->routeIs('admin.settings') ? 'active' : ''}} d-flex align-center fs-14 c-black rad-6 p-10" href="">
-          <i class="fa-solid fa-gear fa-fw"></i>
-          <span>Settings</span>
-        </a>
-      </li>
-      <li>
-        <a class="{{ request()->routeIs('admin.profile') ? 'active' : ''}} d-flex align-center fs-14 c-black rad-6 p-10" href="">
-          <i class="fa-regular fa-user fa-fw"></i>
-          <span>Profile</span>
-        </a>
-      </li>
-      <li>
-        <a class="{{ request()->routeIs('admin.categories') ? 'active' : ''}} d-flex align-center fs-14 c-black rad-6 p-10" href="{{route('admin.categories')}}">
-          <i class="fa-solid fa-diagram-project fa-fw"></i>
-          <span>Categories</span>
-        </a>
-      </li>
-      <li>
-        <a class="{{ request()->routeIs('admin.books') ? 'active' : ''}} d-flex align-center fs-14 c-black rad-6 p-10" href="{{route('admin.books')}}">
-          <i class="fa-solid fa-diagram-project fa-fw"></i>
-          <span>Books</span>
-        </a>
-      </li>
-      <li>
-        <a class="{{ request()->routeIs('admin/reservations') ? 'active' : ''}} d-flex align-center fs-14 c-black rad-6 p-10" href="{{route('admin.reservations')}}">
-          <i class="fa-solid fa-graduation-cap fa-fw"></i>
-          <span>Reservations</span>
-        </a>
-      </li>
+        <li>
+            <a class="{{ request()->routeIs('admin.dashboard') ? 'active' : '' }} d-flex align-center fs-14 c-black rad-6 p-10"
+                href="{{ route('admin.dashboard') }}">
+                <i class="fa-regular fa-chart-bar fa-fw"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <li>
+            <a class="{{ request()->routeIs('admin.settings') ? 'active' : '' }} d-flex align-center fs-14 c-black rad-6 p-10"
+                href="">
+                <i class="fa-solid fa-gear fa-fw"></i>
+                <span>Settings</span>
+            </a>
+        </li>
+        <li>
+            <a class="{{ request()->routeIs('admin.profile') ? 'active' : '' }} d-flex align-center fs-14 c-black rad-6 p-10"
+                href="{{ route('admin.profile') }}">
+                <i class="fa-regular fa-user fa-fw"></i>
+                <span>Profile</span>
+            </a>
+        </li>
+        <li>
+            <a class="{{ request()->routeIs('admin.categories') ? 'active' : '' }} d-flex align-center fs-14 c-black rad-6 p-10"
+                href="{{ route('admin.categories') }}">
+                <i class="fa-solid fa-diagram-project fa-fw"></i>
+                <span>Categories</span>
+            </a>
+        </li>
+        <li>
+            <a class="{{ request()->routeIs('admin.books') ? 'active' : '' }} d-flex align-center fs-14 c-black rad-6 p-10"
+                href="{{ route('admin.books') }}">
+                <i class="fa-solid fa-diagram-project fa-fw"></i>
+                <span>Books</span>
+            </a>
+        </li>
+        <li>
+            <a class="{{ request()->routeIs('admin/reservations') ? 'active' : '' }} d-flex align-center fs-14 c-black rad-6 p-10"
+                href="{{ route('admin.reservations') }}">
+                <i class="fa-solid fa-graduation-cap fa-fw"></i>
+                <span>Reservations</span>
+            </a>
+        </li>
+        <li>
+          <form action="{{ route('logout') }}" method="post">
+              @csrf
+              <button type="submit" class="d-flex align-center fs-14 c-black rad-6 p-10 logout">
+                  <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                  <span>Logout</span>
+              </button>
+          </form>
+        </li>
     </ul>
-  </div>
+</div>
