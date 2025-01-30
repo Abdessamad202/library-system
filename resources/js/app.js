e91362979b34eebc8f9b2e2b101df66e8d27d578
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-30 19:34:46 +0100

    Update app.js with modal functionality for profile editing

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,5 +6,3 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 // font awesome path
 import '@fortawesome/fontawesome-free/css/all.css';
-
-// import 'fontawesome-free/css/all.css';
