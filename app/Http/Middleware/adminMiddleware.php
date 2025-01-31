21ad551949dc1f175ed7a02b2c812a8c90b5c7e4
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-31 23:38:44 +0100

    Add adminMiddleware for access control

--- app/Http/Middleware/adminMiddleware.php
+++ app/Http/Middleware/adminMiddleware.php
@@ -19,6 +19,6 @@ class adminMiddleware
         if (Auth::check() && Auth::user()->role === 'admin') {
             return $next($request);
         }
-        return redirect('/')->with('error', 'You do not have admin access.');
+        return redirect('/login')->with('error', 'You do not have admin access.');
     }
 }
