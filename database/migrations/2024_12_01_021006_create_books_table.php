f0a5a961449316e38c96f2539f42d6447fe460e5
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-31 23:37:46 +0100

    Add migration for creating books table

--- database/migrations/2024_12_01_021006_create_books_table.php
+++ database/migrations/2024_12_01_021006_create_books_table.php
@@ -14,10 +14,9 @@ return new class extends Migration {
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained('categories');
+            $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete();
             $table->string('title');
             $table->string('author');
-            $table->string('edition');
             $table->text('description');
             $table->string('image');
             $table->date('date_edition');
