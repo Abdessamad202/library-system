d37a73e7782ed16cf55974b99d00fc73cbbf88d1
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-31 23:37:50 +0100

    Add books admin view

--- resources/views/admin/books.blade.php
+++ resources/views/admin/books.blade.php
@@ -5,8 +5,19 @@
     <div class="content w-full">
         <x-head />
         <h1 class="p-relative" >Books</h1>
-        <x-books-list :books='$books'/>
+        <x-books-list :books='$books' :categories='$categories'/>
         <x-deleted-books :deletedBooks='$deletedBooks' />
+
+        <!-- Display validation errors -->
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
     </div>
     @session('success')
     <x-notification message="{{ session('success') }}"/>
