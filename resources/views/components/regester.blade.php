df1bc9a95d08524d60901f968cd5d2535d018b0f
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-01 02:30:17 +0100

    Update register.blade.php component

--- resources/views/components/regester.blade.php
+++ resources/views/components/regester.blade.php
@@ -8,21 +8,32 @@
                     <td>Name</td>
                     <td>email</td>
                     <td>profile</td>
+                    <td>Role</td>
                     <td>Date Regestration</td>
+                    <td>Switch Role</td>
                 </tr>
             </thead>
             <!-- Fake Data -->
             <tbody>
                 @foreach ($users as $user)
                     <tr>
-                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->name }} {{ $user->id === auth()->user()->id ? "(you)" : "" }}</td>
                         <td>{{ $user->email }}</td>
-                        <td><img style="width: 50px;height: 50px;border-radius: 50%;" src="{{ Storage::url($user->image)}}" alt=""></td>
+                        <td><img style="width: 50px;height: 50px;border-radius: 50%;"
+                                src="{{ Storage::url($user->image) }}" alt=""></td>
+                        <td>{{ $user->role }}</td>
                         <td>{{ $user->created_at->diffForHumans() }}</td>
+                        <td>
+                            <form action="{{ route('admin.users.change', $user->id) }}" method="post">
+                                @csrf
+                                <button type="submit">
+                                    {{ $user->role === 'user' ? 'Switch To Admin' : 'Switch To User' }}
+                                </button>
+                            </form>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
-            <tbody>
         </table>
     </div>
 </div>
