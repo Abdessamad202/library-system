5336406fca09eea2d3a0dbc8fff8cd023e732493
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-03 00:21:56 +0100

    Added footer.blade.php: Footer component for consistent design

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,3 @@
-
 <footer class="text-center bg-white" style="box-shadow:  0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
     <!-- Grid container -->
     <div class="container pt-4 bg-white">
