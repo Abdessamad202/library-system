ca4a2f5618cb209f84f4cb1a75e906e97f0769cb
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-03 00:21:11 +0100

    Enhanced UserController: Added user CRUD operations and validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,48 +5,15 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Jobs\sendMail;
 use Illuminate\Http\Request;
-use App\Mail\ConfirmationMail;
-use App\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Mail;
-use App\Http\Requests\RegisterRequest;
-use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\Password;
 use App\Http\Requests\UpdateProfileRequest;
 use App\Http\Requests\PasswordUpdateRequest;
 
 class UserController extends Controller
 {
     //
-    public function loginView()
-    {
-        return view("login");
-    }
-    public function register(RegisterRequest $request)
-    {
-        $filables = $request->validated();
-        $filables["password"] = Hash::make($filables["password"]);
-        $user = User::create($filables);
-        $request->session()->regenerate();
-        Auth::login($user);
-        return redirect()->route("home");
-    }
 
-    public function logout(Request $request)
-    {
-        // Log the user out
-        Auth::logout();
-
-        // Invalidate the session to prevent session fixation attacks
-        Session::flush();
-
-        // Regenerate the CSRF token
-        // $request->session()->regenerateToken();
-
-        // Redirect the user to the login page or home page
-        return redirect()->route('login'); // Change 'login' to the appropriate route name
-    }
     public function updateProfile(UpdateProfileRequest $request)
     {
         $filables = $request->validated();
