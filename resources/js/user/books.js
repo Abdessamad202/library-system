976fb55abf35ca15932f577273bda66e9641e66d
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-03 00:21:14 +0100

    Created books.js: Added dynamic functionality for user book interactions

---
+++ resources/js/user/books.js
@@ -0,0 +1,39 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/bundle';
+
+
+let BooksSwiper = new Swiper('.books-slider', {
+  loop: true,
+  // slidesPerView: 3,
+  spaceBetween: 30,
+  grabCursor: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      centeredSlides: true,
+
+    },
+    991: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  }
+});
