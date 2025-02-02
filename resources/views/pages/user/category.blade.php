0b57e797ee12c579859bb7cb251fd96f336d2115
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-03 00:21:58 +0100

    Added category.blade.php: User-specific category page

---
+++ resources/views/pages/user/category.blade.php
@@ -0,0 +1,18 @@
+@extends('../layout.layout')
+@section('title', 'Search')
+@section('links')
+@vite(['resources/css/app.css', 'resources/js/app.js',"resources/js/components/scroll.js","resources/css/components/category.css"])
+@endsection
+@section('content')
+    <x-navbar/>
+    <section class="hero" style="background: url('{{ asset("./assets/banner.jpg") }}') no-repeat center center/cover;">
+        <div class="hero-overlay"></div>
+        <div class="hero-content">
+          <h1>{{ $category->name }}</h1>
+          <p>{{ $category->description }}</p>
+          <button onclick="location.href='#newBooksSlider'">Browse Categories</button>
+        </div>
+      </section>
+    <x-books title="Related Books" :books="$booksRelated"/>
+    <x-footer/>
+@endsection
