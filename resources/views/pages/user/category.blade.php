f0ddbf45b647986cde5dbb244eecdd61f030a18b
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-03 16:45:32 +0100

    Updated category page template for user pages

--- resources/views/pages/user/category.blade.php
+++ resources/views/pages/user/category.blade.php
@@ -1,18 +1,11 @@
 @extends('../layout.layout')
 @section('title', 'Search')
 @section('links')
-@vite(['resources/css/app.css', 'resources/js/app.js',"resources/js/components/scroll.js","resources/css/components/category.css"])
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/components/scroll.js', 'resources/css/user/category.css'])
 @endsection
 @section('content')
-    <x-navbar/>
-    <section class="hero" style="background: url('{{ asset("./assets/banner.jpg") }}') no-repeat center center/cover;">
-        <div class="hero-overlay"></div>
-        <div class="hero-content">
-          <h1>{{ $category->name }}</h1>
-          <p>{{ $category->description }}</p>
-          <button onclick="location.href='#newBooksSlider'">Browse Categories</button>
-        </div>
-      </section>
-    <x-books title="Related Books" :books="$booksRelated"/>
-    <x-footer/>
+    <x-navbar />
+    <x-c-hero-section :category="$category"/>
+    <x-books-section title="Related Books" :books="$booksRelated" />
+    <x-footer />
 @endsection
