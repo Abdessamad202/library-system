812df29b535441d6d8de8c3e78f3a3bbf42dc36d
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-03 16:45:31 +0100

    Enhanced search-bar component for search functionality

---
+++ resources/views/components/search-bar.blade.php
@@ -0,0 +1,20 @@
+<section class="py-3 bg-light ">
+    <form class="max-w-md mx-auto">
+        <div class="relative mb-4 flex  ">
+            <!-- Icon -->
+            <div class="absolute   inset-y-0 right-10 flex items-center pl-3 pointer-events-none">
+                <svg class="w-4 h-4 text-gray-500 glasses" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                    fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                </svg>
+            </div>
+            <!-- Search Input -->
+            <input type="search" id="default-search"
+                class="block w-full py-4 px-10 text-sm text-gray-700 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500"
+                placeholder="Search for books ..." required />
+            <!-- Submit Button -->
+        </div>
+    </form>
+
+</section>
