51e4fb10c4186777b4eddb7a1aa9dbe691563ac4
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-03 16:45:33 +0100

    Enhanced user profile page with additional functionality

--- resources/views/pages/user/profile.blade.php
+++ resources/views/pages/user/profile.blade.php
@@ -1,7 +1,7 @@
 @extends('../layout.layout')
 @section('title', 'Search')
 @section('links')
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/components/scroll.js', 'resources/css/components/profile.css'])
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/components/scroll.js', 'resources/css/user/user-profile.css','resources/js/user/user-profile.js'])
 @endsection
 @section('content')
     <x-navbar />
@@ -11,7 +11,6 @@
     <x-sidebar>
         <div class=" row  w-100 padding-100   mt-5 mb-5 ">
             <div class=" px-4 h-100-vh d-flex justify-content-center align-items-center flex-column">
-                <!-- <h1 class="h2">Change Password</h1> -->
                 <x-profile />
             </div>
         </div>
