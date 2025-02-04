e89aa7107d13641de5bb1c8d8df92129f9f8e9a9
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-04 15:08:44 +0100

    Update books-info.blade.php

--- resources/views/components/books-info.blade.php
+++ resources/views/components/books-info.blade.php
@@ -1,25 +1,47 @@
 @props(['statistics'])
+
 <div class="tickets p-20 bg-white rad-10">
     <h2 class="mt-0 mb-10">Books Statistics</h2>
     <p class="mt-0 mb-20 c-grey fs-15">Everything About Support Books</p>
     <div class="d-flex txt-c gap-20 f-wrap">
+        <!-- Total Books -->
         <div class="box p-20 rad-10 fs-13 c-grey">
-            <i class="fa-solid fa-book-open fa-2x mb-10 c-orange"></i>
+            <i class="fa-solid fa-book fa-2x mb-10 c-orange"></i>
             <span class="d-block c-black fw-bold fs-25 mb-5 count">{{ $statistics['all'] }}</span>
-            Total
+            Total Books
         </div>
+
+        <!-- Available Books -->
         <div class="box p-20 rad-10 fs-13 c-grey">
-            <i class="fa-solid fa-spinner fa-2x mb-10 c-blue"></i>
+            <i class="fa-solid fa-check fa-2x mb-10 c-green"></i>
+            <span class="d-block c-black fw-bold fs-25 mb-5 count">{{ $statistics['available'] ?? 0 }}</span>
+            Available
+        </div>
+
+        <!-- Pending Reservations -->
+        <div class="box p-20 rad-10 fs-13 c-grey">
+            <i class="fa-solid fa-hourglass-half fa-2x mb-10 c-blue"></i>
             <span class="d-block c-black fw-bold fs-25 mb-5 count">{{ $statistics['pending'] }}</span>
-            Pending
+            Pending Reservations
         </div>
+
+        <!-- Reserved Books -->
         <div class="box p-20 rad-10 fs-13 c-grey">
-            <i class="fa-regular fa-circle-check fa-2x mb-10 c-green"></i>
+            <i class="fa-solid fa-bookmark fa-2x mb-10 c-purple"></i>
             <span class="d-block c-black fw-bold fs-25 mb-5 count">{{ $statistics['reserved'] }}</span>
             Reserved
         </div>
+
+        <!-- Returned Books -->
         <div class="box p-20 rad-10 fs-13 c-grey">
-            <i class="fa-regular fa-rectangle-xmark fa-2x mb-10 c-red"></i>
+            <i class="fa-solid fa-undo fa-2x mb-10 c-teal"></i>
+            <span class="d-block c-black fw-bold fs-25 mb-5 count">{{ $statistics['returned'] ?? 0 }}</span>
+            Returned
+        </div>
+
+        <!-- Cancelled Reservations -->
+        <div class="box p-20 rad-10 fs-13 c-grey">
+            <i class="fa-solid fa-times-circle fa-2x mb-10 c-red"></i>
             <span class="d-block c-black fw-bold fs-25 mb-5 count">{{ $statistics['cancelled'] }}</span>
             Cancelled
         </div>
