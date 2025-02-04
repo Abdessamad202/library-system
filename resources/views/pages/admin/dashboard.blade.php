f3bc50ee5b1964af3a6283d88e7a051ef8b8a312
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-04 01:32:06 +0100

    Added dashboard.blade.php for admin panel updates

---
+++ resources/views/pages/admin/dashboard.blade.php
@@ -0,0 +1,21 @@
+@extends('../layouts.admin-layout')
+@section('title', 'Dashboard')
+@section('links')
+    @vite(['resources/js/components/scroll.js', 'resources/js/admin/dashboard.js', 'resources/js/admin/modal.js', 'resources/css/admin/modal.css'])
+@endsection
+@section('content')
+    <x-admin-sidebar />
+    <div class="content w-full">
+        <x-head />
+        <h1 class="p-relative">Dashboard</h1>
+        <x-wrapper>
+            <x-welcome />
+            <x-books-info :statistics='$booksStatistics' />
+        </x-wrapper>
+        <x-regester :users='$newUsers' />
+        <x-reservations :reservations='$todayReservations' />
+    </div>
+    @session('success')
+    <x-notification message="{{ session('success') }}" />
+    @endsession
+@endsection
