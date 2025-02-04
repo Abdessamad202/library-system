8c9ec44177c15cc85d7c7c8782e18f7fd184eee2
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-04 01:32:08 +0100

    Updated home.blade.php to enhance the user home page experience

--- resources/views/pages/user/home.blade.php
+++ resources/views/pages/user/home.blade.php
@@ -1,15 +1,12 @@
-@extends('../layout.layout')
+@extends('../layouts.user-layout')
 @section('title', 'Home')
-@section('links')
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/components/scroll.js'])
-@endsection
 @section('content')
-    <x-navbar/>
+    <x-navbar />
     <x-heroSection />
     <x-books-section title="New Books" :books="$books" />
     <x-location />
     <x-footer />
     @session('success')
-    <x-notification message="{{ session('success') }}"/>
+        <x-notification message="{{ session('success') }}" />
     @endsession
 @endsection
