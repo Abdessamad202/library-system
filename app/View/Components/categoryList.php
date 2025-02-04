98919003ddbf65c81f82c8282307e8948f4461aa
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-04 02:12:57 +0100

    =updating the project structure

--- app/View/Components/categoryList.php
+++ app/View/Components/categoryList.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class categories-list extends Component
+class categoryList extends Component
 {
     /**
      * Create a new component instance.
@@ -21,6 +21,6 @@ class categories-list extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.categories-list');
+        return view('components.category-list');
     }
 }
--- resources/views/pages/admin/categories.blade.php
+++ resources/views/pages/admin/categories.blade.php
@@ -9,7 +9,7 @@
     <div class="content w-full">
         <x-head />
         <h1 class="p-relative" >Categories</h1>
-        <x-categories-list :categories='$categories' />
+        <x-category-list :categories='$categories' />
         {{-- <x-books-list :books='$books'/>
         <x-deleted-books :deletedBooks='$deletedBooks' /> --}}
     </div>
