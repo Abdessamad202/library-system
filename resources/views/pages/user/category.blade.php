5c64e560e85ed9f8c2d70a798bf06d58fe0d4cc5
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-04 01:32:07 +0100

    Updated category.blade.php for better category display on user side

--- resources/views/pages/user/category.blade.php
+++ resources/views/pages/user/category.blade.php
@@ -1,11 +1,11 @@
-@extends('../layout.layout')
+@extends('../layouts.user-layout')
 @section('title', 'Search')
 @section('links')
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/components/scroll.js', 'resources/css/user/category.css'])
+    @vite(['resources/css/user/category.css'])
 @endsection
 @section('content')
     <x-navbar />
-    <x-c-hero-section :category="$category"/>
+    <x-c-hero-section :category="$category" />
     <x-books-section title="Related Books" :books="$booksRelated" />
     <x-footer />
 @endsection
