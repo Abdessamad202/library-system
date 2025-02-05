07ed17d0d758294c858f8c9ff8c3ad1e0a30f061
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-05 20:27:11 +0100

    Updated reservation page with new reservation features

--- resources/views/pages/user/reservation.blade.php
+++ resources/views/pages/user/reservation.blade.php
@@ -9,7 +9,8 @@
         <div class="row  w-100 padding-100   mt-5 mb-5 ">
             <div class="py-5 px-4 d-flex justify-content-center align-items-center flex-column">
                 <!-- <h1 class="h2">Change Password</h1> -->
-                <x-recent :recent="$recent" />
+                <x-recent :recent="$recent" title="Recent Reservations" />
+                <x-recent :recent="$reserved" title="Reserved Books" />
                 <x-history :history="$history" />
             </div>
         </div>
