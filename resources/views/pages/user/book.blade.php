01ef475e4a18beafeb4323f905421238f9b40a28
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-05 20:27:11 +0100

    Updated book page for enhanced user book reservation experience

--- resources/views/pages/user/book.blade.php
+++ resources/views/pages/user/book.blade.php
@@ -5,7 +5,7 @@
 @endsection
 @section('content')
     <x-navbar />
-    <x-book :book="$book" />
-    <x-books-section title="Related Books" :books="$relatedBooks" />
+    <x-book :book="$book" :userReservations="$userReservations" />
+    <x-books-section title="Related Books" :books="$relatedBooks"  />
     <x-footer />
 @endsection
