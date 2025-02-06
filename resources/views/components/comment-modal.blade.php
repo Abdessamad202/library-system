3e2a4dfde835cc523b58252679a478cc3006bc08
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-06 07:21:51 +0100

    Updated comment-modal component blade for displaying comments modally

---
+++ resources/views/components/comment-modal.blade.php
@@ -0,0 +1,24 @@
+<div class="modal fade" id="editCommentModal" tabindex="-1" aria-labelledby="editCommentModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="editCommentModalLabel">Edit Comment</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form method="post">
+                    @csrf
+                    @method('PUT')
+                    <div class="mb-3">
+                        <label for="comment-text" class="col-form-label">Comment:</label>
+                        <textarea class="form-control" name="comment" id="comment-text"></textarea>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Send message</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
