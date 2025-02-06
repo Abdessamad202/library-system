5dca63be90fbe651da587b7e18463ba67a3e9607
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-06 18:44:14 +0100

    Enhanced book component blade to include like button integration

--- resources/views/components/book.blade.php
+++ resources/views/components/book.blade.php
@@ -1,8 +1,12 @@
-@props(['book', 'userReservations'])
+@props(['book', 'userReservations', 'likes'])
 <section class="py-5">
     <div class="container px-4 px-lg-5 my-5 text-md-start text-center">
         <div class="row gx-4 gx-lg-5 align-items-center justify-content-lg-center justify-content-md-between g-4">
-            <div class="col-md-5 d-flex justify-content-center">
+            <div class="col-md-5 d-flex justify-content-center book-cover">
+                <button class='like-btn {{ $likes['is_liked'] ? 'liked' : ''}}' title="{{ $likes['is_liked'] ? 'Unlike' : 'Like'}}" data-book-id="{{ $book->id }}">
+                    <i class="fa-solid fa-heart "></i>
+                    <span class="like-count">{{ $likes['count'] }}</span>
+                </button>
                 <img class="card-img-top mb-5 mb-md-0 w-75" src="{{ asset($book->image) }}" alt="{{ $book->title }}" />
             </div>
             <div class="col-md-6">
@@ -12,8 +16,10 @@
                     @if ($book->reserved)
                         <div class="alert alert-success text-center w-100" role="alert">
                             <h4 class="alert-heading fw-bold">Reserved Successfully</h4>
-                            <p class="mb-0">You have successfully reserved this book. You can now check your reservations in the "My Reservations" section.</p>
-                            <a id="comment" href="{{ route('reservation') }}" class="btn btn-primary mt-1" type="button">
+                            <p class="mb-0">You have successfully reserved this book. You can now check your
+                                reservations in the "My Reservations" section.</p>
+                            <a id="comment" href="{{ route('reservation') }}" class="btn btn-primary mt-1"
+                                type="button">
                                 <i class="fa-solid fa-circle-plus"></i>
                                 Check Reservations
                             </a>
@@ -22,13 +28,15 @@
                         @if ($book->reserved_number === $book->stock)
                             <div class="alert alert-danger text-center w-100" role="alert">
                                 <h4 class="alert-heading fw-bold">Book Unavailable</h4>
-                                <p class="mb-0">Unfortunately, this book is currently out of stock. Please check back later or explore other titles.</p>
+                                <p class="mb-0">Unfortunately, this book is currently out of stock. Please check back
+                                    later or explore other titles.</p>
                             </div>
                         @else
                             @if ($userReservations >= 3)
                                 <div class="alert alert-warning text-center w-100" role="alert">
                                     <h4 class="alert-heading fw-bold">Reservation Limit Reached</h4>
-                                    <p class="mb-0">You have already reserved the maximum number of books allowed. Complete an existing reservation to reserve more.</p>
+                                    <p class="mb-0">You have already reserved the maximum number of books allowed.
+                                        Complete an existing reservation to reserve more.</p>
                                 </div>
                             @else
                                 <form action="" method="post">
