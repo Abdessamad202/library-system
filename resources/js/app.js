12b587c3842a2bacf822c44d5467730c2b8a5fbb
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-06 07:21:47 +0100

    Updated app.js with new JavaScript functionality for the application

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,10 @@
 // import './bootstrap';
-import $ from 'jquery';
-
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 // font awesome path
 import '@fortawesome/fontawesome-free/css/all.css';
+// Import Bootstrap CSS
+// Import Bootstrap JavaScript (Ensure it's after Bootstrap CSS)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // 'bootstrap.bundle' includes Popper.js
+
