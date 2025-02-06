b8606e6454f41a2cc2c31a206d0dc50f8eea14ad
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-06 07:20:26 +0100

    Updated BookController to manage book-related functionalities

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -23,23 +23,13 @@ class BookController extends Controller
     }
     public function update(BookRequest $request, Book $book)
     {
-        // Get validated data from the request
         $data = $request->validated();
-
-        // Check if a new image file was uploaded
+        $data['is_commentable'] = $request->is_commentable ? 1 : 0;
         if ($request->hasFile('image')) {
-            // Store the new image and get its path
             $data['image'] = $request->file('image')->store('books', 'public');
             $data['image'] = '/storage/' . $data['image'];
-        } else {
-            // If no new image, retain the existing image
-            $data['image'] = $book->image;
         }
-
-        // Update the book with the data
         $book->update($data);
-
-        // Redirect back with a success message
         return redirect()->route('admin.books')->with('success', 'Book updated successfully');
     }
 
