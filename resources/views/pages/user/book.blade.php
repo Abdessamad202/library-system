f92b3fd08f57dcb2394214448c9566ffc2553aad
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-06 07:21:53 +0100

    Updated user book page blade for enhanced layout and features

--- resources/views/pages/user/book.blade.php
+++ resources/views/pages/user/book.blade.php
@@ -1,11 +1,17 @@
 @extends('../layouts.user-layout')
 @section('title', 'Search')
 @section('links')
-    @vite(['resources/js/user/book.js'])
+    @vite(['resources/js/user/book.js', 'resources/css/user/book.css'])
 @endsection
 @section('content')
     <x-navbar />
     <x-book :book="$book" :userReservations="$userReservations" />
+    @if($book->is_commentable)
+        <x-comments :comments="$comments" :book="$book" />
+    @endif
     <x-books-section title="Related Books" :books="$relatedBooks"  />
     <x-footer />
+    @session('success')
+        <x-notification message="{{ session('success') }}" />
+    @endsession
 @endsection
